d44886a0806d798888c9171eadddd3d021d63899
Debashis Biswas <debashis.biswas@asu.edu>
2021-12-20 16:38:46 -0700

    new test using Counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use counter::Counter;
 use rand::distributions::Uniform;
 use rand::{prelude::StdRng, Rng, SeedableRng};
 use std::collections::HashMap;
@@ -49,26 +50,36 @@ fn duplicate_encode_capacity(text: &str) -> String {
     return result;
 }
 
+fn duplicate_encode_counter(text: &str) -> String {
+    let char_counts = text.chars().collect::<Counter<_>>();
+    let mut result = String::with_capacity(text.len());
+    for c in text.chars() {
+        let count = char_counts[&c];
+        result.push(if count == 1 { '(' } else { ')' });
+    }
+    return result;
+}
+
 fn test_functions(functions: Vec<NamedFunction>) {
     println!(
         "Counting {} characters per test (over {} tests)",
         CHARS_PER_TEST, NUMBER_OF_TEST_RUNS
     );
 
-    println!("Running tests...");
     print!("Generating random input...");
     let start = Instant::now();
     let random = StdRng::seed_from_u64(42);
     let range = Uniform::new_inclusive(b'0', b'z');
-    let input_word_chunk: String = random
+    let input_word_chunk = random
         .sample_iter(&range)
         .take(INPUT_WORD_SIZE)
         .map(char::from)
-        .collect();
-    let input_word = input_word_chunk.repeat(INPUT_WORD_NUM);
-    let input_word = &input_word.as_str();
+        .collect::<String>()
+        .repeat(INPUT_WORD_NUM);
+    let input_word = &input_word_chunk.as_str();
     println!(" took {} ms.", start.elapsed().as_millis());
 
+    println!("Running tests...");
     for f in functions {
         let start = Instant::now();
         for _ in 0..NUMBER_OF_TEST_RUNS {
@@ -88,6 +99,10 @@ fn main() {
         NamedFunction {
             name: "duplicate_encode_capacity",
             body: duplicate_encode_capacity,
+        },
+        NamedFunction {
+            name: "duplicate_encode_counter",
+            body: duplicate_encode_counter,
         },
     ]);
     test_functions(functions);
